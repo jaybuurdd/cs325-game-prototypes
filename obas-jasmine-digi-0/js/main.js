d73addbb6ac405bb984ea1445add1c9a47044523
jobas <jobas@gmu.edu>
2021-02-04 11:18:10 -0500

    Changed image to funny one punch man pic.

--- obas-jasmine-digi-0/js/main.js
+++ obas-jasmine-digi-0/js/main.js
@@ -21,7 +21,7 @@ class MyScene extends Phaser.Scene {
     
     preload() {
         // Load an image and call it 'logo'.
-        this.load.image( 'logo', 'assets/monkey-d-luffy.png' );
+        this.load.image( 'logo', 'assets/download.png' );
     }
     
     create() {
