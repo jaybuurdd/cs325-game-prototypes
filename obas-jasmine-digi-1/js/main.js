e8e61b8fbe47b4d23dde54a183520f5e4ac7ac30
jobas <jobas@gmu.edu>
2021-02-08 23:24:22 -0500

    Digital Assignment 1 draft, first attempt pushing code for mouse control with swinging sword gif and background.

---
+++ obas-jasmine-digi-1/js/main.js
@@ -0,0 +1,59 @@
+import "./phaser.js";
+
+// You can copy-and-paste the code from any of the examples at https://examples.phaser.io here.
+// You will need to change the `parent` parameter passed to `new Phaser.Game()` from
+// `phaser-example` to `game`, which is the id of the HTML element where we
+// want the game to go.
+// The assets (and code) can be found at: https://github.com/photonstorm/phaser3-examples
+// You will need to change the paths you pass to `this.load.image()` or any other
+// loading functions to reflect where you are putting the assets.
+// All loading functions will typically all be found inside `preload()`.
+
+// The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
+
+class MyScene extends Phaser.Scene {
+    
+    constructor() 
+    {
+        super();
+        this.move = 0;
+        this.x = 0;
+        this.y = 0;
+    }
+    
+    preload() {
+        // Load an image and call it 'logo'.
+        this.load.image( 'background', 'assets/parallax-mountain-bg.png' );
+        this.load.image('sword', 'assets/sword_spin.gif' )
+    }
+    
+    create() {
+        this.add.image(0, 0, 'background')
+            .setOrigin(0);
+        this.group = this.add.group({key: 'sword', frameQuantity: 128});
+        this.input.on('pointermove', function (pointer){
+            this.x = pointer.x;
+            this.y = pointer.y;
+        }, this);
+        }
+    }
+    
+    update(time, delta) {
+      this.move += delta;
+      if(this.move > 6)
+      {
+          Phaser.Actions.ShiftPosition(this.group.getChildren(), this.x, this.y);
+          this.move = 0;
+      }  
+
+    }
+}
+
+const game = new Phaser.Game({
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 800,
+    height: 600,
+    scene: MyScene,
+    physics: { default: 'arcade' },
+    });
